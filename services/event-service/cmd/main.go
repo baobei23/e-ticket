06f0eadc63ad1ab12ba8cb3c2017b5988c611336
baobei23 <reginaldsaja98@gmail.com>
2026-01-10 21:52:35 +0700

    feat: implement gracefulshutdown to event-service

--- services/event-service/cmd/main.go
+++ services/event-service/cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/baobei23/e-ticket/services/event-service/internal/infrastructure/grpc"
 	"github.com/baobei23/e-ticket/services/event-service/internal/infrastructure/repository"
@@ -26,8 +29,19 @@ func main() {
 	grpcServer := grpcserver.NewServer()
 	grpc.NewEventHandler(grpcServer, service)
 
-	fmt.Printf("Event Service listening on :50051\n")
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		fmt.Printf("Event Service listening on :50051\n")
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-stop
+	log.Println("\nShutting down Event Service...")
+
+	grpcServer.GracefulStop()
+	log.Println("Event Service exited properly")
 }
