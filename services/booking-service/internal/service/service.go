8447d1d2180a8fc030ffd92c72aed70d4576dd71
baobei23 <reginaldsaja98@gmail.com>
2026-04-10 21:31:29 +0700

    refactor: enhance CreateBooking method to include seat reservation and release logic

--- services/booking-service/internal/service/service.go
+++ services/booking-service/internal/service/service.go
@@ -26,19 +26,22 @@ func NewBookingService(repo domain.BookingRepository, eventProvider domain.Event
 	}
 }
 
+// main flow of creating a booking
 func (s *BookingService) CreateBooking(ctx context.Context, userID int64, eventID int64, quantity int32) (*domain.Booking, string, error) {
 
-	isAvailable, unitPrice, err := s.eventProvider.CheckAvailability(ctx, eventID, quantity)
-	if err != nil {
-		return nil, "", err
+	bookingID := uuid.New().String()
+
+	if err := s.eventProvider.ReserveSeat(ctx, eventID, quantity, bookingID); err != nil {
+		return nil, "", fmt.Errorf("failed to reserve seat: %w", err)
 	}
 
-	if !isAvailable {
-		return nil, "", errors.New("insufficient seats")
+	_, unitPrice, err := s.eventProvider.CheckAvailability(ctx, eventID, quantity)
+	if err != nil {
+		s.eventProvider.ReleaseSeat(ctx, eventID, quantity, bookingID)
+		return nil, "", fmt.Errorf("failed to check availability: %w", err)
 	}
 
 	totalAmount := unitPrice * float64(quantity)
-	bookingID := uuid.New().String()
 
 	booking := &domain.Booking{
 		ID:          bookingID,
@@ -51,24 +54,19 @@ func (s *BookingService) CreateBooking(ctx context.Context, userID int64, eventI
 	}
 
 	if err := s.repo.Create(ctx, booking); err != nil {
-		return nil, "", err
-	}
-
-	// TODO: outbox pattern
-
-	if err := s.publisher.PublishBookingCreated(ctx, booking); err != nil {
-		// TODO: outbox pattern
+		s.eventProvider.ReleaseSeat(ctx, eventID, quantity, bookingID)
+		return nil, "", fmt.Errorf("failed to create booking: %w", err)
 	}
 
 	paymentURL, err := s.paymentProvider.CreatePayment(ctx, bookingID, userID, totalAmount, unitPrice, quantity)
 	if err != nil {
-		// Log error, but booking is created
-		// User should be able to retry payment later (Endpoint GetBookingDetail should return paymentURL also if not paid),
-		// for simplicity, we return error or empty string.
+		s.eventProvider.ReleaseSeat(ctx, eventID, quantity, bookingID)
 		return nil, "", fmt.Errorf("failed to create payment session: %w", err)
 	}
+
 	return booking, paymentURL, nil
 }
+
 func (s *BookingService) GetBookingDetail(ctx context.Context, bookingID string, userID int64) (*domain.Booking, error) {
 	booking, err := s.repo.GetByID(ctx, bookingID)
 	if err != nil {
